873f359040db86168b928b47d632c2d4bbdd21ad
PRIYANKA <PRIYANKA@LAPTOP-98SB48LL.mshome.net>
2026-04-21 12:48:40 +0530

    initial commit

---
+++ src/test/java/git_pack/createPack1Test.java
@@ -0,0 +1,18 @@
+package git_pack;
+
+import org.testng.annotations.Test;
+
+public class createPack1Test {
+	@Test
+	public void createPrice() {
+		System.out.println("Execute Pricecontact Test");
+	}
+	
+	@Test
+	public void modifyPrice() {
+		System.out.println("Execute modifycontact Test");
+		
+	}
+
+
+}
---
+++ src/test/java/git_pack/createpack2Test.java
@@ -0,0 +1,16 @@
+package git_pack;
+
+import org.testng.annotations.Test;
+
+public class createpack2Test {
+	
+	@Test
+	public void createSellTest() {
+		System.out.println("execute createSellTest");
+	}
+	@Test
+	public void modifySellTest() {
+		System.out.println("execute modifySellTest");
+	}
+
+}
